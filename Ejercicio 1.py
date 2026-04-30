552740bf2e141bf6c9fee46132ea31fb6c94e7e4
Gertrudiz Carvajal <gertrudiscarvajalcuchimba2018@gmail.com>
2026-04-29 22:33:35 -0500

    Implemntacion de la documentacion de las líneas 11, 14,18,22, 26, 27, 28, 30, 31 del codigo y la creacion de la clase abstracta de manera correcta.

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -8,17 +8,26 @@ import datetime
 from abc import ABC, abstractmethod
 
 # GESTIÓN DE ERRORES ESPECÍFICOS 
-class ErrorSistemaFJ(Exception): 
+class ErrorSistemaFJ(Exception): # Define la clase base para errores del sistema
     """ clase donde se identifican todos los errores dentro del sistema"""
     pass
-class DatosInvalidosError(ErrorSistemaFJ): 
+class DatosInvalidosError(ErrorSistemaFJ): # Excepción cuando los datos fallan
     """ Indica cuando los datos de entrada no cumplen las validaciones"""
     pass
 
-class ServicioNoDisponibleError(ErrorSistemaFJ): 
+class ServicioNoDisponibleError(ErrorSistemaFJ): # Excepción para servicios inactivos
     """ Indica cuando el servicio solicitado no puede ser procesado"""
     pass
 
-class ErrorFinanciero(ErrorSistemaFJ): 
+class ErrorFinanciero(ErrorSistemaFJ): # Excepción para errores de cálculos
     """ Indica inconsistencias en cálculos de costos o descuentos"""
     pass
+#  CREACION DE LA CLASE ABSTRACTA
+class EntidadSistema(ABC):
+    def __init__(self):
+        self.fecha_registro = datetime.datetime.now()
+
+    @abstractmethod
+    def mostrar_detalle(self):
+        """ Garantiza la salida de información de cada subclase de manera correcta"""
+        pass
