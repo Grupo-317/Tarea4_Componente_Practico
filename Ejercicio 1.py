284822115bc5e60ebd88f09c6b93749f16af8336
Gertrudiz Carvajal <gertrudiscarvajalcuchimba2018@gmail.com>
2026-04-29 22:12:00 -0500

    Implementacion de las cuatro clases para la empresa llamada Sottware FJ. ATT: Gertrudiz Carvjal Cuchimba

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -4,4 +4,21 @@
 # sobrecargados, manejo de listas internas y validaciones estrictas,
 # demostrando un diseño orientado a objetos completamente funcional.
 
+import datetime
+from abc import ABC, abstractmethod
 
+# GESTIÓN DE ERRORES ESPECÍFICOS 
+class ErrorSistemaFJ(Exception): 
+    """ clase donde se identifican todos los errores dentro del sistema"""
+    pass
+class DatosInvalidosError(ErrorSistemaFJ): 
+    """ Indica cuando los datos de entrada no cumplen las validaciones"""
+    pass
+
+class ServicioNoDisponibleError(ErrorSistemaFJ): 
+    """ Indica cuando el servicio solicitado no puede ser procesado"""
+    pass
+
+class ErrorFinanciero(ErrorSistemaFJ): 
+    """ Indica inconsistencias en cálculos de costos o descuentos"""
+    pass
